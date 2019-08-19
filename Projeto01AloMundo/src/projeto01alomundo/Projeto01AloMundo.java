475d7db16ee9028fcbb03b9a9cdae4e2ee129bd5
Marlon Rodler <35037476+MarlonAMR@users.noreply.github.com>
2019-08-19 19:40:19 -0300

    Inicialização do Projeto01AloMundo

---
+++ Projeto01AloMundo/src/projeto01alomundo/Projeto01AloMundo.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto01alomundo;
+
+/**
+ *
+ * @author aluno
+ */
+public class Projeto01AloMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        /*
+        coments 
+        de mais
+        linhas
+        */
+        
+       
+        System.out.println("Alooow");
+        
+
+        
+        
+        
+    }
+    
+}
